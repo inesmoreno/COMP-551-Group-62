d0382aa9e469d9439db15b380d539e737e70e91e
Julien-Mounth <47541168+Julien-Mounth@users.noreply.github.com>
2020-02-08 21:34:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 pd.set_option('max_columns', 120)
 pd.set_option('max_colwidth', 5000)
@@ -105,71 +106,129 @@ iris_array = iris_2020.to_numpy()
 #################################################################################
 
 class LogisticRegression:
-  def __init__(self, lr=0.01, epsilon=1e-2, fit_intercept=True, verbose=False):
+  #def __init__(self, lr=0.01, epsilon=1e-2, fit_intercept=True, verbose=False):
+  def __init__(self, lr=0.01, epsilon=1e-2, fit_intercept=True, max_iter=1000):
     self.lr = lr
     self.epsilon = epsilon
     self.fit_intercept = fit_intercept
+    self.max_iter = max_iter
+    self.w = np.zeros(1)
+    self.proba = np.zeros(1)
+    self.labels = np.zeros(1)
 
-  def sigmoid(z):
+  def sigmoid(self, z):
     h = 1 / (1 + np.exp(-z))
     return h
   
-  def cost(h, y):
+  def cost(self, h, y):
     J = np.mean((-y * np.log(h) - (1-y) * np.log(1 - h)))
     return J
 
-  def gradient(X,y,w):
+  def gradient(self, X, y, w):
     N, D = X.shape
-    z = np.dot(X,w)
-    yh = sigmoid(z)
+    z = np.dot(X, self.w)
+    yh = self.sigmoid(z)
     grad = np.dot(X.T, yh - y) / N
     return grad
   
-  def GradientDescent (X, y, lr, epsilon):
-    N,D = X.shape
-    w = np.zeros(D)
+  def GradientDescent(self, X, y, lr, epsilon):
+    N, D = X.shape
+    self.w = np.zeros(D)
     g = np.inf
-    while np.linalg.norm(g) > eps:
-      g = gradient(X, y,w)
-      w = w - lr*g 
+    k = 0
+    while (np.linalg.norm(g) > epsilon) and (k < self.max_iter):
+      g = self.gradient(X, y, self.w)
+      self.w = self.w - lr*g 
 
-      z = np.dot(X,w)
-      h = sigmoid(z)
-      print(f'loss: {cost(h,y)} \t')
+      z = np.dot(X, self.w)
+      h = self.sigmoid(z)
+      print(f'loss: {self.cost(h, y)} \t')
 
-    return w
+      k += 1
+    return self.w
 
-  def fit (self, X, y):
-    GradientDescent(X,y,lr,epsilon)
+  def fit(self, X, y):
+    self.GradientDescent(X, y, self.lr, self.epsilon)
+  
+  def predict_prob(self, X):
+    self.proba = self.sigmoid(np.dot(X, self.w))
+    return self.proba
 
-  def predict_prob(X):
-    return sigmoid(X, self.w)
-
-  def predict(X, threshold):
-    return predict_prob(X) >= threshold
+  def predict(self, X, threshold):
+    self.predict_prob(X)
+    return np.multiply(self.proba >= threshold, 1)
 
 
 # Gaussian Naive Bayes
 class NaiveBayes:
-  def __init__(training, test):
+  def __init__(self, training, test):
     self.__training = training
     self.__test = test
     self.__n = len(training)
     
-  def fit(X,y,Xtest):
-    N,C = y.shape
+  def fit(self, X, y, Xtest):
+    N, C = y.shape
     D = X.shape[1]
-    mu, s = np.zeros((C,D)), np.zeros((C,D))
+    mu, s = np.zeros((C, D)), np.zeros((C, D))
     for c in range(C):
-      inds = np.nonzero(y[:,c])[0]
-      mu[c,:] = np.std(X[inds,:],0)
-      s[c,:] = np.std(X[inds,:],0)
-    log_prior = np.log(np.mean(y,0))[:,None]
-    log_likelihood = - np.sum(np.log(s[:,None,:]) + .5*(((Xt[None,:,:] - mu[:,None,:])/s[:,None,:])**2),2)
+      inds = np.nonzero(y[:, c])[0]
+      mu[c, :] = np.std(X[inds, :], 0)
+      s[c, :] = np.std(X[inds, :], 0)
+    log_prior = np.log(np.mean(y, 0))[:, None]
+    log_likelihood = - np.sum(np.log(s[:, None, :]) + .5*(((X[None, :, :] - mu[: , None, :])/s[:, None, :])**2), 2)
     return log_prior + log_likelihood
 
-   def predict(yh, y):
+  def predict(self, yh, y):
+     return 0
    	
+
+#################################################################################
+#####                                METHODS                                #####
+#################################################################################
+
+def count_result(target_labels, true_labels):
+    tp, fp, tn, fn = 0, 0, 0, 0
+    for i in range(len(target_labels)):
+        if target_labels[i] == 0:
+            if true_labels[i] == 0:
+                tn += 1
+            if true_labels[i] == 1:
+                fn += 1
+        if target_labels[i] == 1:
+            if true_labels[i] == 0:
+                fp += 1
+            if true_labels[i] == 1:
+                tp += 1
+    return tp, fp, tn, fn
+
+def accuracy(tp, fp, tn, fn):
+    return (tp + tn)/(tp + fp + tn + fn)
+
+def error_rate(tp, fp, tn, fn):
+    return (fp + fn)/(tp + fp + tn + fn)
+
+def precision(tp, fp, tn, fn):
+    return tp/(tp + fp)
+
+def recall(tp, fp, tn, fn):
+    return tp/(tp + fn)
+
+def f1score(tp, fp, tn, fn):
+    return 2 * (tp/(tp + fp)) * (tp/(tp + fn)) / ((tp/(tp + fp)) + (tp/(tp + fn)))
+
+def cross_validation(X, y, classifier, k=5, shuffle=False, obj=accuracy):
+  if shuffle:
+    np.random.shuffle
+  N, D = X.shape
+  index = np.linspace(0, N, k+1, False, dtype=np.int)
+  total_obj = 0
+  for i in range(k):
+    classifier.fit(np.vstack([X[:index[i], :], X[index[i+1]:, :]]), np.hstack([y[:index[i]], y[index[i+1]:]]))
+    target_labels = classifier.predict(X[index[i]:index[i+1], :], 0.5)
+    tp, fp, tn, fn = count_result(target_labels, y[index[i]:index[i+1]])
+    total_obj += obj(tp, fp, tn, fn)
+  return total_obj/k
+
 
 #################################################################################
 #####                          DATASET DESCRIPTION                          #####
@@ -181,8 +240,94 @@ class NaiveBayes:
 #####                                TRAINING                               #####
 #################################################################################
 
+dataset = ionosphere_array
+np.random.shuffle(dataset)
+
+train_percent = 0.8
+
+N, D = dataset.shape
+
+train_size = math.floor(N*train_percent)
+training_set = dataset[:train_size, :]
+testing_set = dataset[train_size:, :]
+
+
+# Finding the best learning rate
+
+lr_lst = []
+acc_lst = []
+best_acc = 0
+best_lr = 0
+
+for i in range(50):
+  logreg = LogisticRegression(lr = 0.001 + 0.001*i)
+  acc = cross_validation(training_set[:, :-1], training_set[:, -1], logreg)
+  if acc > best_acc:
+    best_acc = acc
+    best_lr = 0.001 + 0.001*i
+  lr_lst.append(0.001 + 0.001*i)
+  acc_lst.append(acc)
+
+print("Best Learning Rate : ", best_lr)
+
+plt.plot(lr_lst, acc_lst)
+plt.suptitle("Accuracy in function of the learning rate")
+plt.xlabel("Learning Rate")
+plt.ylabel("Accuracy")
+plt.show()
+
+
+# Finding the best epsilon
+
+eps_lst = []
+acc_lst = []
+best_acc = 0
+best_eps = 0
+
+for i in range(10):
+  logreg = LogisticRegression(epsilon = 0.001 + 0.001*i)
+  acc = cross_validation(training_set[:, :-1], training_set[:, -1], logreg)
+  if acc > best_acc:
+    best_acc = acc
+    best_eps = 0.001 + 0.001*i
+  eps_lst.append(0.001 + 0.001*i)
+  acc_lst.append(acc)
+
+print("Best Epsilon : ", best_eps)
+
+plt.plot(eps_lst, acc_lst)
+plt.suptitle("Accuracy in function of the epsilon")
+plt.xlabel("Epsilon")
+plt.ylabel("Accuracy")
+plt.show()
+
+
+# Finding the best number of iteration
+
+iter_lst = []
+acc_lst = []
+best_acc = 0
+best_iter = 0
+
+for i in range(50):
+  logreg = LogisticRegression(max_iter = 100 + 100*i)
+  acc = cross_validation(training_set[:, :-1], training_set[:, -1], logreg)
+  if acc > best_acc:
+    best_acc = acc
+    best_iter = 100 + 100*i
+  iter_lst.append(100 + 100*i)
+  acc_lst.append(acc)
+
+print("Best Number of Iteration : ", best_iter)
+
+plt.plot(iter_lst, acc_lst)
+plt.suptitle("Accuracy in function of the number of iteration")
+plt.xlabel("Number of iteration")
+plt.ylabel("Accuracy")
+plt.show()
 
 
 #################################################################################
 #####                             RESULT ANALYSIS                           #####
 #################################################################################
+
